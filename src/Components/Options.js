370388e4f4ac8e226f83729bc4e8c8964c460816
Sonu <sonu3660@gmail.com>
2020-08-30 16:49:46 +0530

    Added Options Component

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -1,25 +1,26 @@
-import React from 'react';
-/**
- * Options component with a button and event handler
- */
-class Options extends React.Component{
-    //https://image.flaticon.com/icons/svg/715/715750.svg
-    constructor(props){
-        super(props)
-        this.handler=this.handler.bind(this);
-    }
-    handler(e){
-        e.stopPropagation();
-        e.preventDefault();
-        if(this.props.handler)
-            this.props.handler(e);
-    }
-    render(){
-        const {src,alt}=this.props;
-        return(
-            <div className="icon" onClick={this.handler}><img src={src} alt={alt} style={this.props.style}/></div>
-        )
-    }
+import React from "react";
+
+// Options component with a button and event handler
+class Options extends React.Component {
+   constructor(props) {
+      super(props);
+      this.handler = this.handler.bind(this);
+   }
+
+   handler(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      if (this.props.handler) this.props.handler(e);
+   }
+
+   render() {
+      const { src, alt } = this.props;
+      return (
+         <div className="icon" onClick={this.handler}>
+            <img src={src} alt={alt} style={this.props.style} />
+         </div>
+      );
+   }
 }
 
 export default Options;
