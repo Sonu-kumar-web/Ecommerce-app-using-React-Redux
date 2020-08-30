c0ac60d80b7e6111479463caed250cd595f9baf3
Sonu <sonu3660@gmail.com>
2020-08-30 16:54:26 +0530

    Add config file

--- src/config/configStore.js
+++ src/config/configStore.js
@@ -1,17 +1,11 @@
-import {createStore, applyMiddleware} from 'redux';
-import root from '../reducers/index';
-import thunk from 'redux-thunk';
-import{persistStore} from'redux-persist'
+import { createStore, applyMiddleware } from "redux";
+import root from "../reducers/index";
+import thunk from "redux-thunk";
+import { persistStore } from "redux-persist";
 
-// configure store and make cart item persistant using redux-persist
-
-function configStore(){
-  
-    let store=createStore(
-        root,
-        applyMiddleware(thunk)
-      );
-    return {store,persistor:persistStore(store)}
+function configStore() {
+   let store = createStore(root, applyMiddleware(thunk));
+   return { store, persistor: persistStore(store) };
 }
 
 export default configStore;
